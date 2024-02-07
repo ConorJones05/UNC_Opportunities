3067d6e989a7302e5f74932326b0f92aebe712e6
Conor Jones <155279919+ConorJones05@users.noreply.github.com>
2024-02-06 23:44:36 -0500

    scraper v1

--- UCS_2024_Spring/tutiton_scraper.py
+++ UCS_2024_Spring/tutiton_scraper.py
@@ -1,8 +1,10 @@
 """UCS Tution Cost Datascraper"""
+import pandas as pd
+from bs4 import BeautifulSoup  
 
-
-
-
+url = 'UCS_2024_Spring/COLLEGE MATRICULATION 2018–2023_ Durham Academy  - Sheet1.csv'
+df = pd.read_csv(url,index_col=0)
+print(df)
 
 
 
