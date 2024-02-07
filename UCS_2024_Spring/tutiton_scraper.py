a137dbd65953269004a251f0886e3cb42e4e3512
Conor Jones <155279919+ConorJones05@users.noreply.github.com>
2024-02-06 23:58:07 -0500

    added file in

--- UCS_2024_Spring/tutiton_scraper.py
+++ UCS_2024_Spring/tutiton_scraper.py
@@ -1,11 +1,11 @@
 """UCS Tution Cost Datascraper"""
 import pandas as pd
 from bs4 import BeautifulSoup  
-
 url = 'UCS_2024_Spring/COLLEGE MATRICULATION 2018–2023_ Durham Academy  - Sheet1.csv'
 df = pd.read_csv(url,index_col=0)
+df['Amout of Students'].fillna(1, inplace=True)
+df['Amout of Students'] = df['Amout of Students'].astype(int)
 print(df)
-
 
 
 
