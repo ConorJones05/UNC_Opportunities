857e25d8b17281545301b065b34b8aa4054783af
Conor Jones <155279919+ConorJones05@users.noreply.github.com>
2024-02-07 22:32:16 -0500

    ML

--- BattleshipAI/ML_logic.py
+++ BattleshipAI/ML_logic.py
@@ -1,8 +1,3 @@
-import numpy as np
-import random
-import sympy as sy
-import matplotlib.pyplot as plt
-from datascience import *
 from battleship_game import BattleshipGame
 
 BattleshipGame().build_board()
