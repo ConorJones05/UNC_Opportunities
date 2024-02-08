b102eb1854633d589c0193fa5536d564a2b249d9
Conor Jones <155279919+ConorJones05@users.noreply.github.com>
2024-02-08 13:26:54 -0500

    started model

---
+++ UCS_2024_Spring/longterm_model.py
@@ -0,0 +1,24 @@
+def damian_base(year):
+    if year < 1:
+        return 65_000
+    else:
+        return 360_000
+melissa = 155_000	
+melissa_bonus = 85_000
+inflation = 1.025
+school_inflation = 1.05
+    
+def with_raise(person, year: int):
+    raise_arary = []
+    for i in range(year):
+        increase = person * inflation 
+        raise_arary.append(increase)
+
+
+
+
+
+
+    
+
+
