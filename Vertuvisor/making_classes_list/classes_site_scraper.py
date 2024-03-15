59bfdadb6b010f84020d759ecffc0be7bdc52e95
ConorJones05 <conorjones05@gmail.com>
2024-03-15 01:02:19 -0400

    working scraper

--- Vertuvisor/making_classes_list/classes_site_scraper.py
+++ Vertuvisor/making_classes_list/classes_site_scraper.py
@@ -27,29 +27,30 @@ def find_all_elements():
         page2 = requests.get(url)
         soup2 = BeautifulSoup(page2.content, "html.parser")
         courseblocks = soup2.find_all('div', class_='courseblock')
+        codes_array = []
+        name_array = []
+        dec_array = []
+        pre_array_basic = []
+
         for block in range(len(courseblocks)):
-            course_code = soup2.find_all(class_="text detail-code margin--tiny text--semibold text--big")
-            codes_array = []
-            for clean_codes in course_code:
-                cleaned_codes = clean_codes.get_text(strip=True)[:-1]
-                codes_array.append(cleaned_codes)
-            course_name = soup2.find_all(class_="text detail-title margin--tiny text--semibold text--big")
-            name_array = []
-            for clean_titles in course_name:
-                cleaned_titles = clean_titles.get_text(strip=True)[:-1]
-                name_array.append(cleaned_titles)
-            course_dec = soup2.find_all(class_="courseblockextra")
-            dec_array = []
-            for clean_dec in course_dec:
-                cleaned_codes = clean_dec.get_text(strip=True)[:-1].lower()
-                dec_array.append(cleaned_codes)
-            course_pre = soup2.find_all(class_="text detail-requisites margin--default")
-            pre_array_basic = []
-            tester = 0
-            for pre in range(len(course_pre)):
-                pre_clean = course_pre[pre].get_text(strip=True)[:-1]
-                pre_array_basic.append(pre_clean)
-    return pre_array_basic, tester            
+            course_code = courseblocks[block].find(class_="text detail-code margin--tiny text--semibold text--big")
+            codes_array.append(course_code.get_text(strip=True)[:-1])
+
+            course_name = courseblocks[block].find(class_="text detail-title margin--tiny text--semibold text--big")
+            name_array.append(course_name.get_text(strip=True)[:-1])
+
+            course_dec = courseblocks[block].find(class_="courseblockextra")
+            if course_dec:
+                dec_array.append(course_dec.get_text(strip=False).lower())
+            else: 
+                dec_array.append(None)
+
+            course_pre = courseblocks[block].find(class_="text detail-requisites margin--default")
+            if course_pre:
+                pre_array_basic.append(course_pre.get_text(strip=True)[:-1])
+            else:
+                pre_array_basic.append(None)
+    return codes_array, name_array, dec_array, pre_array_basic   
 
 
 def dict_builder():
